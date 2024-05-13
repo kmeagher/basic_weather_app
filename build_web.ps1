34a188d3bf0494eab8f05413b970b88e53ab7274
Meagher <33063126+kmeagher@users.noreply.github.com>
2024-05-13 12:09:05 -0400

    updated build commands to include custom icon

--- build_web.ps1
+++ build_web.ps1
@@ -1,2 +1,4 @@
 flutter build web
+flutter pub run flutter_launcher_icons
+Get-ChildItem -Path public -Include *.* -File -Recurse | ForEach-Object { $_.Delete()}
 xcopy build\web public /h /i /c /k /e /r /y
